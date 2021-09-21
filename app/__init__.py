bf7ba1f8684b9ddbe4bc4b90c9c74fe3be26812b
ken <ken.mbira@student.moringaschool.com>
2021-09-21 08:11:16 +0300

    connected to the database

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,10 @@
-from flask import Flask
+from flask import Flask, config
+from flask_sqlalchemy import SQLAlchemy
+
+from config import config_options
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
+app.config.from_object(config_options['development'])
+db.init_app(app)
--- config.py
+++ config.py
@@ -1,4 +1,19 @@
+from flask import config
+
+
 class Config:
     """This defines the factors that are same for the whole application
     """
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kenmbira:1234@localhost/pizza_shop'
+
+class DevConfig(Config):
+    """This defines the configurations in development
+
+    Args:
+        Config ([type]): [description]
+    """
+
+
+config_options = {
+    'development':DevConfig
+}
