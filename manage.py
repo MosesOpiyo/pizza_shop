68558e5cc27dc78baea327e0af074c3e571fa4f9
ken <ken.mbira@student.moringaschool.com>
2021-09-21 08:53:06 +0300

    add the user and admin roles to the table

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
 
-from app.models import User
+from app.models import Roles, User
 from app import app,db
 
 manager = Manager(app)
@@ -11,7 +11,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db,User = User,Roles = Roles)
 
 if __name__ == '__main__':
     manager.run()
