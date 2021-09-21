c57fa69303bf0f6b4a2b8ce259e302016b5ead8b
ken <ken.mbira@student.moringaschool.com>
2021-09-21 09:40:40 +0300

    add an orders table that will compile the links from all the other tables

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import backref, relationship
 from . import db
 
 class User(db.Model):
@@ -12,6 +13,7 @@ class User(db.Model):
     username = db.Column(db.String(255))
     location = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    order = db.relationship('Orders',backref = 'user', lazy="dynamic")
 
     def __repr__(self):
         return f'User {self.username}'
@@ -25,7 +27,7 @@ class Roles(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    users = db.relationship('User',backref = 'roles',lazy="dynamic")
 
 class Pizza(db.Model):
     """This defines the behaviours of an individual pizza
@@ -37,6 +39,7 @@ class Pizza(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     name= db.Column(db.String(255))
     base_price = db.Column(db.Integer)
+    order = db.relationship('Orders',backref = 'pizzas', lazy="dynamic")
 
 class Size(db.Model):
     """This will define the aspects of size
@@ -48,6 +51,7 @@ class Size(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
     price = db.Column(db.Integer)
+    order = db.relationship('Orders',backref = 'sizes', lazy="dynamic")
 
 class Toppings(db.Model):
     """This will define the topping aspects
@@ -59,3 +63,19 @@ class Toppings(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
     price = db.Column(db.Integer)
+    order = db.relationship('Orders',backref = 'toppings', lazy="dynamic")
+
+class Orders(db.Model):
+    """This is 
+
+    Args:
+        db ([type]): [description]
+    """
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer,primary_key = True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    role_id = db.Column(db.Integer,db.ForeignKey('pizza.id'))
+    role_id = db.Column(db.Integer,db.ForeignKey('size.id'))
+    role_id = db.Column(db.Integer,db.ForeignKey('toppings.id'))
+    price = db.Column(db.Integer)
+    quantity = db.Column(db.Integer)
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
 
-from app.models import Pizza, Roles, Size, Toppings, User
+from app.models import Orders, Pizza, Roles, Size, Toppings, User
 from app import app,db
 
 manager = Manager(app)
@@ -11,7 +11,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User,Roles = Roles,Pizza = Pizza,Size = Size,Toppings = Toppings)
+    return dict(app = app,db = db,User = User,Roles = Roles,Pizza = Pizza,Size = Size,Toppings = Toppings,Orders = Orders)
 
 if __name__ == '__main__':
     manager.run()
