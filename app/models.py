a1373ef59332d1bde51a8964761524364b46864f
ken <ken.mbira@student.moringaschool.com>
2021-09-21 08:42:19 +0300

    add a roles table

--- app/models.py
+++ app/models.py
@@ -11,6 +11,18 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     location = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Roles(db.Model):
+    """This defines the users roles
+
+    Args:
+        db ([type]): [description]
+    """
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
